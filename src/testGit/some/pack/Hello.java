5e9deef3d151498f78d6e39d799a5fa53c8bc3d0
Mitrxen <61155721+Mitrxen@users.noreply.github.com>
2021-07-26 20:31:18 +0500

    Update Hello.java
    Add !!

--- src/testGit/some/pack/Hello.java
+++ src/testGit/some/pack/Hello.java
@@ -2,6 +2,6 @@ package testGit.some.pack;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello This New Brave World!");
+		System.out.println("Hello This New Brave World!!!");
 	}
 }
